56017ac3b18242a1fde23caed993cf5288a0b08d
Zamin Alili <zaminalili29@gmail.com>
2023-01-12 10:17:54 +0400

    electronics shop solution

---
+++ ElectronicsShop/ElectronicsShop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicsShop
+{
+    public class Program
+    {
+
+        /*
+         Problem: https://www.hackerrank.com/challenges/electronics-shop/problem
+         */
+
+        /*
+     * Complete the getMoneySpent function below.
+     */
+        static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+
+            List<int> allSpentSum = new List<int>();
+
+            foreach (var keyboard in keyboards)
+            {
+                if (keyboard < b)
+                {
+                    foreach (var drive in drives)
+                    {
+                        if (drive < b && keyboard + drive <= b)
+                        {
+                            allSpentSum.Add(keyboard + drive);
+                        }
+
+                    }
+                }
+          
+            }
+
+            return allSpentSum.Count>0 ? allSpentSum.Max() : -1;
+        }
+
+        static void Main(string[] args)
+        {
+            //int[] keyboards = { 40, 50, 60};
+            //int[] drives = { 5, 8, 12};
+            //int b = 60;
+
+            int[] keyboards = { 4 };
+            int[] drives = { 5 };
+            int b = 5;
+
+            Console.WriteLine(getMoneySpent(keyboards, drives, b));
+        }
+    }
+}
