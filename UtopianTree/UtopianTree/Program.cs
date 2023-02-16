bd96025cef80ddcd11904d08525304952fd076a5
Zamin Alili <zaminalili29@gmail.com>
2023-02-16 11:06:58 +0400

    utopian tree solution

---
+++ UtopianTree/UtopianTree/Program.cs
@@ -0,0 +1,20 @@
+﻿// Problem: https://www.hackerrank.com/challenges/utopian-tree/problem
+
+static int utopianTree(int n)
+{
+	int total = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+            total++;
+		else
+            total = total * 2;
+	}
+
+    return total;
+}
+
+int n = 4;
+
+Console.WriteLine(utopianTree(n));
