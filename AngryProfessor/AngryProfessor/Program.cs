9fe561189dbc551c4c4bc596346d9b628e93f863
Zamin Alili <zaminalili29@gmail.com>
2023-02-18 14:52:45 +0400

    angry professor solution

---
+++ AngryProfessor/AngryProfessor/Program.cs
@@ -0,0 +1,12 @@
+﻿// Problem: https://www.hackerrank.com/challenges/angry-professor/problem
+
+static string angryProfessor(int k, List<int> a)
+{
+	int arrivedOnTime = 0;
+
+    foreach (int item in a)
+		if(item <= 0)
+			arrivedOnTime++;
+
+    return arrivedOnTime >= k ? "NO" : "YES";
+}
