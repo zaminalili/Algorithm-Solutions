ee3f464ba8295bcb809637ee069d7ee1e7f3efe2
Zamin Alili <zaminalili29@gmail.com>
2023-02-20 14:56:03 +0400

    add viral advertising solution

---
+++ ViralAdvertising/ViralAdvertising/Program.cs
@@ -0,0 +1,21 @@
+﻿// Problem: https://www.hackerrank.com/challenges/strange-advertising/problem
+
+
+static int viralAdvertising(int n)
+{
+	int shared = 5;
+	int liked;
+	int cumulative = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		liked = shared / 2;
+		shared = liked * 3;
+		cumulative += liked;
+	}
+
+    return cumulative;
+}
+
+
+Console.WriteLine(viralAdvertising(5));
