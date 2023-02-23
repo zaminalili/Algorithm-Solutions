ce158458c619554b98855da62f324ff67f6df232
Zamin Alili <zaminalili29@gmail.com>
2023-02-23 09:17:12 +0400

    add save the prisoner solution

---
+++ SaveThePrisoner/SaveThePrisoner/Program.cs
@@ -0,0 +1,15 @@
+﻿// Problem: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+static int saveThePrisoner(int n, int m, int s)
+{
+        int result = (m + s - 1) % n;
+
+        return result == 0 ? n : result;
+}
+
+
+Console.WriteLine(saveThePrisoner(208526924, 756265725, 150817879));
+
+Console.WriteLine(saveThePrisoner(962975336, 972576181, 396355184));
+
+Console.WriteLine(saveThePrisoner(5,10,1));
