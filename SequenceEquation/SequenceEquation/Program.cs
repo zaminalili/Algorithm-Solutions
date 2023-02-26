b9baa32e26db0ce91876e7f4c624731a8e781418
Zamin Alili <zaminalili29@gmail.com>
2023-02-26 22:31:23 +0400

    add sequence equation solution

---
+++ SequenceEquation/SequenceEquation/Program.cs
@@ -0,0 +1,15 @@
+﻿// Problem: https://www.hackerrank.com/challenges/permutation-equation/problem
+
+static List<int> permutationEquation(List<int> p)
+{
+    List<int> y = new List<int>();
+
+    for (int i = 1; i <= p.Count; i++)
+        y.Add(p.IndexOf(p.IndexOf(i) + 1) + 1);
+
+    return y;
+}
+
+List<int> p = new List<int> { 5, 2, 1, 3, 4 };
+
+permutationEquation(p);
