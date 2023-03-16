52b7d70edf2a5f0ce7b0c554f99a78ce203354d2
Zamin Alili <zaminalili29@gmail.com>
2023-03-16 18:15:10 +0400

    Add extra long factorial solution

---
+++ ExtraLongFactorials/ExtraLongFactorials/Program.cs
@@ -0,0 +1,15 @@
+﻿// Problem: https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+using System.Numerics;
+
+static void extraLongFactorials(int n)
+{
+	BigInteger result = new BigInteger(n);
+
+	for (int i = n-1; i > 0; i--)
+		result *= new BigInteger(i);
+
+	Console.WriteLine(result);
+}
+
+extraLongFactorials(100);
