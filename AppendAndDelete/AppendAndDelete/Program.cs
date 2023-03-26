39f93c70372f34abff53cebebdcd3776c6873ce8
Zamin Alili <zaminalili29@gmail.com>
2023-03-26 12:07:44 +0400

    Add append and delete solution

---
+++ AppendAndDelete/AppendAndDelete/Program.cs
@@ -0,0 +1,26 @@
+﻿static string appendAndDelete(string s, string t, int k)
+{
+	int sameChar = 0;
+	int sLength = s.Length;
+	int tLength = t.Length;
+	int result;
+	
+    for (int i = 0; i < (sLength < tLength ? sLength : tLength); i++)
+     {
+        if (s[i] == t[i])
+             sameChar++;
+        else
+             break;
+    }
+
+    result = sLength + tLength - 2 * sameChar;
+
+
+    return k >= sLength + tLength || k >= result && (k - result) % 2 == 0 ? "Yes" : "No";
+}
+
+string s = "aaaaaaaaaa";
+string t = "aaaaa";
+int k = 7;
+
+Console.WriteLine(appendAndDelete(s, t, k));
