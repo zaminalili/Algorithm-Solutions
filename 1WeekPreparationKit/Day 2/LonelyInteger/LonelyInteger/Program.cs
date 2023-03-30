6f1c8390103f17d4233006ffdb3433e9827dc2af
Zamin Alili <zaminalili29@gmail.com>
2023-03-30 06:53:38 +0400

    Add lonely integer solution

---
+++ 1WeekPreparationKit/Day 2/LonelyInteger/LonelyInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿static int lonelyinteger(List<int> a)
+{
+    while (true)
+    {
+        int e = a[0];
+        a.RemoveAt(0);
+
+        if (!Contains(a, e))
+            return e;
+        else
+            a.Remove(e);
+    }   
+}
+
+static bool Contains(List<int> arr, int item)
+{
+    
+    foreach (int e in arr)
+        if(item == e)
+            return true;
+
+    return false;
+}
+
+List<int> a = new List<int>() { 1, 2, 3, 4, 3, 2, 1 };
+
+Console.WriteLine(lonelyinteger(a));
