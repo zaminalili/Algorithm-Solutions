726eb15cc834d5f646fd8989345d16dedc66e40b
Zamin Alili <zaminalili29@gmail.com>
2023-04-05 09:44:14 +0400

    Add sherlock solution

---
+++ SherlockAndSquares/Program.cs
@@ -0,0 +1,10 @@
+﻿static int squares(int a, int b)
+{
+    return Convert.ToInt32(Math.Floor(Math.Sqrt(b)) - Math.Ceiling(Math.Sqrt(a))+1);
+}
+
+int a = 100;
+int b = 1000;
+
+Console.WriteLine(squares(a, b));
+
